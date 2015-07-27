8e9d72c3c8f78eef55f71f47f4cb3d7a4e6571ce
Sebastian Lenz <github@sebastian-lenz.de>
2015-07-27 12:42:54 +0200

    Move images to assets directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,12 +113,12 @@ module.exports = function (grunt) {
         },
 		sprite:{
 			default: {
-				src: 'src/assets/images/**/*.png',
+				src: 'src/assets/sprites/**/*.png',
 				dest: 'src/files/assets/images/sprites.png',
 				destCss: 'src/assets/styles/dt/_sprites.sass',
 				imgPath: '../images/sprites.png',
 				padding: 2,
-				retinaSrcFilter: 'src/assets/images/**/*-2x.png',
+				retinaSrcFilter: 'src/assets/sprites/**/*-2x.png',
 				retinaDest: 'src/files/assets/images/sprites-2x.png',
 				retinaImgPath: '../images/sprites-2x.png'
 			}
@@ -133,7 +133,7 @@ module.exports = function (grunt) {
                 tasks: ['ts']
             },
 			sprite: {
-				files: ['src/assets/images/**/*'],
+				files: ['src/assets/sprites/**/*'],
 				tasks: ['sprite']
 			}
         }
